1fdc80667080890a513c9e0ba916762beb4d2157
Rana Hisham <hishamrana00@gmail.com>
2022-12-13 16:48:58 +0200

    Initial Setup is done (some packages, dependencies and files)

---
+++ app/src/main/java/com/rana/mymovieapplication/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rana.mymovieapplication.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rana.mymovieapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
