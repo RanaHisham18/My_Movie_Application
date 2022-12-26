5ee97316fbe750a258c6ef4e2af06f67d5921fef
Rana Hisham <hishamrana00@gmail.com>
2022-12-26 11:37:58 +0200

    modify koin dependency

---
+++ app/src/main/java/com/rana/mymovieapplication/app/Application.kt
@@ -0,0 +1,31 @@
+package com.rana.mymovieapplication.app
+import android.app.Application
+import android.content.Context
+import coil.Coil
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val imageLoader = ImageLoader.Builder(this).componentRegistry {
+            add(SvgDecoder(this@Application))
+        }.build()
+
+        Coil.setImageLoader(imageLoader)
+startKoin {
+    androidContext(this@Application)
+    modules(
+        listOf(
+            //here I should mention the app modules
+        )
+    )
+}
+    }
+
+}
