db62e0e30ddc322170d2679b0b37b348b640b58c
Rana Hisham <hishamrana00@gmail.com>
2023-02-09 10:50:17 +0200

    add, edit some files

---
+++ app/src/main/java/com/rana/mymovieapplication/data/remote/entities/MovieReviewsModel.kt
@@ -0,0 +1,32 @@
+package com.rana.mymovieapplication.data.remote.entities
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class MovieReviewsModel(
+    val id: Int,
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+{
+    @JsonClass(generateAdapter = true)
+    data class Result(
+        val author: String,
+        val author_details: AuthorDetails,
+        val content: String,
+        val created_at: String,
+        val id: String,
+        val updated_at: String,
+        val url: String
+    )
+
+    @JsonClass(generateAdapter = true)
+    data class AuthorDetails(
+        val avatar_path: String?,
+        val name: String?,
+        val rating: Double?,
+        val username: String?
+    )
+}
---
+++ app/src/main/java/com/rana/mymovieapplication/services/MovieReviewsService.kt
@@ -0,0 +1,27 @@
+package com.rana.mymovieapplication.services
+
+import com.rana.mymovieapplication.data.remote.entities.MovieReviewsModel
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieReviewsService {
+
+
+    @GET("movie/{movie_id}")
+    fun getReviews(
+        @Path("movie_id")
+        movie_id: Long,
+        @Query("api_key")
+        apikey: String,
+        @Query("language")
+        language: String = "en-US",
+        @Query("page")
+        page: Int
+    ): Single<MovieReviewsModel>
+
+
+
+}
+
